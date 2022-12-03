33002caa287e4a2f4825b7df8a1636acd6773996
Muhammad Guntur Arfianto <72558038+TRUEnder@users.noreply.github.com>
2022-12-03 14:46:23 +0700

    Delete serverChat.js

--- serverChat.js
+++
@@ -1,8 +0,0 @@
-const server = require('./server')
-const io = require('socket.io')(server)
-
-io.on('connection', (socket) => {
-    socket.on('send-chat-message', (roomId, message) => {
-        socket.to(roomId).emit('chat-message', message)
-    })
-})
